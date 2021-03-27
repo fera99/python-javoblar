7bcd9f3e42ff955a0b45c6a5a82249ae4ecf4a32
unknown <81424279+fera99@users.noreply.github.com>
2021-03-27 15:36:02 +0500

    yangilandi

--- 1.py
+++ 1.py
@@ -1,20 +1,20 @@
-#otam = {'ism':'Bobir', 'yil':'1975', 'manzil':'Samarqand'}
-#onam = {'ism':'Malika', 'yil':'1979', 'manzil':'Toshkent'}
-#print(f"Otamning ismi {otam['ism']}, {otam['yil']}-yili {otam['manzil']}da tug\'ilgan.")
-#print(f"Onamning ismi {onam['ism']}, {onam['yil']}-yili {onam['manzil']}da tug\'ilgan")
+otam = {'ism':'Bobir', 'yil':'1975', 'manzil':'Samarqand'}
+onam = {'ism':'Malika', 'yil':'1979', 'manzil':'Toshkent'}
+print(f"Otamning ismi {otam['ism']}, {otam['yil']}-yili {otam['manzil']}da tug\'ilgan.")
+print(f"Onamning ismi {onam['ism']}, {onam['yil']}-yili {onam['manzil']}da tug\'ilgan")
 
-#food = {'onam':'somsa', 'otam':'osh', 'akam':'shurva', 'ukam':'manti', 'opam':'honim'}
-#print(f"Otamning sevimli ovqati {food['otam']}")
-#print(f"Onamning sevimli ovqati {food['onam']}")
-#print(f"Akamning sevimli ovqati {food['akam']}")
-#print(f"Ukamning sevimli ovqati {food['ukam']}")
-#rint(f"Opamning sevimli ovqati {food['opam']}")
+food = {'onam':'somsa', 'otam':'osh', 'akam':'shurva', 'ukam':'manti', 'opam':'honim'}
+print(f"Otamning sevimli ovqati {food['otam']}")
+print(f"Onamning sevimli ovqati {food['onam']}")
+print(f"Akamning sevimli ovqati {food['akam']}")
+print(f"Ukamning sevimli ovqati {food['ukam']}")
+rint(f"Opamning sevimli ovqati {food['opam']}")
 
-#atama = {'if':'agar', 'else':'aks holda', 'float':'o\'nli son', 'integer':'butun son', 'elif':'if teskarisi'}
-#print(atama['if'])
-#print(atama['integer'])
-#print(atama['else'])
-#print(atama['float'])
+atama = {'if':'agar', 'else':'aks holda', 'float':'o\'nli son', 'integer':'butun son', 'elif':'if teskarisi'}
+print(atama['if'])
+print(atama['integer'])
+print(atama['else'])
+print(atama['float'])
 
 lugat = {'olma':'apple', 'gilos':'cherry', 'banan':'banana', 'bodring':'cucumber', 'pomidor':'tomatola', 'enangniki':'ye ongangni ... sani'}
 
